278bec1d10da99b5d5460c4498fc8ed48333904e
Aoxcis <gregoirepaul@laposte.net>
2024-10-09 11:50:15 +0200

    v3 finie manque image site

--- SitePro/v3/fr/projets.php
+++ SitePro/v3/fr/projets.php
@@ -2,20 +2,24 @@
 <body>
     <div class="flex-menu-content">
         <div class="content">
-            <h2>Projets : </h2>
-            <div class="projet">
+        <h2>Projets : </h2>
+            <div class="projets">
                 
-                <h3>Space Invaders</h3>
-                <p>Voici un projet de jeu en pharo</p>
-                <video width="480" height="360" controls>
-                    <source src="videos/SpaceInvaders.mp4" type="video/mp4">
-                </video>
+                <div class="projet">
+                    
+                    <h3>Space Invaders</h3>
+                    <p>Voici un projet de jeu en pharo</p>
+                    <video width="480" height="360" controls>
+                        <source src="videos/SpaceInvaders.mp4" type="video/mp4">
+                    </video>
+                </div>
+
+                <div class="projet">
+                    <h3>SitePro</h3>
+                    <p>Voici un projet de site web en html et css</p>
+                </div> 
             </div>
 
-            <div class="projet">
-                <h3>SitePro</h3>
-                <p>Voici un projet de site web en html et css</p>
-            </div> 
             <div class="langue">
                 <a href="index.php?page=projets&lang=en">🇬🇧 Anglais</a>
             </div>
