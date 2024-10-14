e11b503174fbd0071ee520e37cbd95fba67dad33
Aoxcis <gregoirepaul@laposte.net>
2024-10-14 14:28:17 +0200

    ajout de wordpress pour le cours pas de stress

--- tp4/exo3/init_db.php
+++ tp4/exo3/init_db.php
@@ -1,23 +1,23 @@
 <?php
+    require_once('config.php');
 
-require_once('config.php');
+    $script = file_get_contents('sql/create_db.sql');
 
-$script = file_get_contents('sql/create_db.sql');
+    $connectionString = "mysql:host=". _MYSQL_HOST;
+    if(defined('_MYSQL_PORT')){
+        $connectionString .= ";port=". _MYSQL_PORT;
+    }
+    $connectionString .= ";dbname=". _MYSQL_DBNAME;
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
 
-$connectionString = "mysql:host=". _MYSQL_HOST;
-if(defined('_MYSQL_PORT')){
-    $connectionString .= ";port=". _MYSQL_PORT;
-}
-$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
-
-$pdo = NULL;
-try {
-    $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = NULL;
+    try {
+        $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $erreur) {
+        echo 'Erreur : '.$erreur->getMessage();
+    }
     $pdo->query($script);
-}
-catch (PDOException $erreur) {
-    echo 'Erreur : '.$erreur->getMessage();
-}
 
-
+    
