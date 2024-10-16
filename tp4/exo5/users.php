96d39405720b25ba44f22500c5e54bfe44155442
Aoxcis <gregoirepaul@laposte.net>
2024-10-16 09:54:03 +0200

    gestion doublons et inexistants

--- tp4/exo5/users.php
+++ tp4/exo5/users.php
@@ -7,7 +7,30 @@ function get_users($db){
     return $res;
 }
 
+function user_exists($db, $login){
+    $sql = "SELECT * FROM users WHERE login = :login";
+    $exe = $db->prepare($sql);
+    $exe->bindParam(':login', $login);
+    $exe->execute();
+    if($exe->rowCount() > 0){
+        http_response_code(400);
+        exit(json_encode(['status' => 'error', 'message' => 'User already exists']));
+    }
+}
+
+function user_not_exists($db, $login){
+    $sql = "SELECT * FROM users WHERE login = :login";
+    $exe = $db->prepare($sql);
+    $exe->bindParam(':login', $login);
+    $exe->execute();
+    if($exe->rowCount() == 0){
+        http_response_code(400);
+        exit(json_encode(['status' => 'error', 'message' => 'User does not exist']));
+    }
+}
+
 function add_user($db, $login, $email){
+    user_exists($db, $login);
     $sql = "INSERT INTO users (login, email) VALUES (:login, :email)";
     $exe = $db->prepare($sql);
     $exe->bindParam(':login', $login);
@@ -16,6 +39,7 @@ function add_user($db, $login, $email){
 }
 
 function delete_user($db, $login){
+    user_not_exists($db, $login);
     $sql = "DELETE FROM users WHERE login = :login";
     $exe = $db->prepare($sql);
     $exe->bindParam(':login', $login);
@@ -23,6 +47,7 @@ function delete_user($db, $login){
 }
 
 function update_user($db, $previous_login, $login, $email){
+    user_not_exists($db, $previous_login);
     $sql1 = "SELECT id FROM users WHERE login = :previous_login";
     $exe1 = $db->prepare($sql1);
     $exe1->bindParam(':previous_login', $previous_login);
@@ -53,7 +78,6 @@ switch($_SERVER["REQUEST_METHOD"]) {
         exit(json_encode($result));
     case 'POST':
         $input = json_decode(file_get_contents('php://input'), true);
-
         if (isset($input['login']) && isset($input['email'])) {
             setHeaders();
             add_user($pdo, $input['login'], $input['email']);
